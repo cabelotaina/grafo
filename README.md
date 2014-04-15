4e066d530ef13a30edbc98b94899472f459bbb36
Maurilio Atila <cabelotaina@gmail.com>
2014-04-15 20:16:50 -0300

    grafo não direcionado e read-me

--- README.md
+++ README.md
@@ -1,4 +1,31 @@
-grafo
+Grafo
 =====
 
-Implementação de um grafo em ruby e implementação do algoritmo de caminho minimo de Dikjstra ;)
+Implementação da classe grafo, grafo não orientado, em ruby e solução de um problema por meio da implementação do algoritmo de caminho minimo de Dikjstra ;)
+
+Definição
+---------
+
+De acordo com o professor Antonio Carlos Mariani INE/UFSC um *grafo*, não orientado, visite [página com representações de grafos](http://www.inf.ufsc.br/grafos/represen/), possui as seguintes ações básicas:
+
+G.adicionaVértice(v) 	"Adiciona um novo vértice em G"
+G.removeVértice(v) 	"Remove um  vértice de G, juntamente com todas as conexões"
+G.conecta(v1,v2) 	"Conecta os vértices v1 e v2 em G"
+G.desconecta(v1,v2) 	"Desconecta os vértices v1 e v2 em G"
+G.ordem Inteiro 	"Retorna o número de vértices de G"
+G.vértices Conjunto 	"Retorna um conjunto contendo os vértices de G"
+G.umVértice Vertice 	"Retorna um vértice qualquer de G"
+G.adjacentes(v)Conjunto "Retorna um conjunto contendo os vértices adjacentes a v em G"
+G.grau(v)Inteiro 	"Retorna o número de vértices adjacentes a v em G"
+
+Descrição
+---------
+
+Neste trabalho implementamos uma classe Grafo em ruby, esta classe será utilizada pelo algoritimo de caminho minimo de Dikjstra para solucionar o problema X.
+
+TODO
+----
+
+* Implementar o algoritimo de Dijkstra para calculo do caminho minimo;
+* Permitir adicionar peso durante a inserção de uma nova aresta;
+* Definir um problema para ser tratado com este algoritimo, [lista de problemas](http://www.inf.ufsc.br/grafos/problemas/); 
---
+++ grafo.rb
@@ -0,0 +1,62 @@
+class Grafo
+	def initialize
+		# usuario deve passar argumento informando se o grafo é direcionado ou não?
+		# atualmente o algoritimo é apenas para grafo simples		
+		@vertices = Hash.new
+	end
+	# "Adiciona um novo vértice em G"
+	# G.adicionaVértice(v) 	
+	def adicionaVertice(vertice)
+		@vertices[vertice] = Hash.new	
+	end
+	# "Remove um  vértice de G, juntamente com todas as conexões"
+	# G.removeVértice(v)
+	def removeVertice(vertice)
+		# iterar sobre o vertice e ver quais vertices devem remover uma entrada dele;
+		@vertices[vertice].keys.each{|v| @vertices[v].delete vertice}
+		@vertices.delete vertice
+	end
+	# "Conecta os vértices v1 e v2 em G"
+	# G.conecta(v1,v2)
+	def conectar(vertice1,vertice2)
+		# se for usarmos dijkstra temos que passar peso positivo como parametro.
+		@vertices[vertice1][vertice2] = "peso?"
+		@vertices[vertice2][vertice1] = "peso?"
+	end
+	# "Desconecta os vértices v1 e v2 em G"
+	# G.desconecta(v1,v2)
+	def desconectar(vertice1, vertice2)
+		@vertices[vertice1].delete vertice2
+		@vertices[vertice2].delete vertice1	
+	end
+	# "Retorna o número de vértices de G"
+	# G.ordem → Inteiro 
+	def ordem
+		@vertices.size
+	end
+	# "Retorna um conjunto contendo os vértices de G"
+	# G.vértices → Conjunto 
+	def vertices
+		@vertices.keys
+	end
+	# "Retorna um vértice qualquer de G"
+	# G.umVértice → Vertice	
+	def umVertice
+		@vertices.keys[rand(@vertices.size)]
+	end
+	# "Retorna um conjunto contendo os vértices adjacentes a v em G"
+	# G.adjacentes(v)Conjunto
+	def adjacentes(vertice)
+		@vertices[vertice].keys
+	end
+	# "Retorna o número de vértices adjacentes a v em G"
+	# G.grau(v)Inteiro
+	def grau(vertice)
+		if @vertices[vertice][vertice]
+			@vertices[vertice].size+1
+		else
+			@vertices[vertice]
+		end
+	end
+
+end
